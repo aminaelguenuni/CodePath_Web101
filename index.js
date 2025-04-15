dfb9a73f1d717aed68abe9f2d27134a117e89999
Amina EL Guenuni <88779300+aminaelguenuni@users.noreply.github.com>
2025-04-15 10:41:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,13 +17,53 @@ const toggleDarkMode = () => {
     // This section will run whenever the button is clicked
 }
 
-// Step 3: Register a 'click' event listener for the theme button,
-//             and tell it to use toggleDarkMode as its callback function
 themeButton.addEventListener("click", toggleDarkMode);
 
 
 
 /*** Form Handling [PLACEHOLDER] [ADDED IN UNIT 6] ***/
-/*** Form Validation [PLACEHOLDER] [ADDED IN UNIT 7] ***/
+// Step 1: Query for the submit button
+const rsvpButton = document.getElementById('rsvp-button');
+
+// Step 2: Create the addParticipant function
+function addParticipant(event) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const state = document.getElementById('state').value;
+
+    const participantInfo = document.createElement('p');
+    participantInfo.textContent = `🎟️ ${name} from ${state} has RSVP'd.`;
+
+    document.querySelector('.rsvp-participants').appendChild(participantInfo);
+    document.getElementById('rsvp-form').reset();
+}
+
+const validateForm = () => {
+    event.preventDefault(); 
+    let containsErrors = false; 
+    const rsvpInputs = document.getElementById("rsvp-form").elements;
+    
+    // Loop through all inputs to validate them
+    for (let i = 0; i < rsvpInputs.length; i++) {
+        const input = rsvpInputs[i];
+        
+        // Skip the submit button
+        if (input.type === 'submit') continue;
+        
+        // Check if input is too short
+        if (input.value.length < 2) {
+            containsErrors = true;
+            input.classList.add('error');
+        } else {
+            input.classList.remove('error');
+        }
+    }
+
+    if (containsErrors === false) {
+        addParticipant();
+    }
+}
+
+rsvpButton.addEventListener('click', validateForm);
 /*** Animations [PLACEHOLDER] [ADDED IN UNIT 8] ***/
 /*** Success Modal [PLACEHOLDER] [ADDED IN UNIT 9] ***/
